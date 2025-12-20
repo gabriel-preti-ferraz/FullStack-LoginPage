8ba86374d5c05f5bb251640d68805dd2a55c5b49
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-19 21:22:02 -0300

    feat: PassRecovery HTML

--- frontend/src/pages/PassRecovery.jsx
+++ frontend/src/pages/PassRecovery.jsx
@@ -1,8 +1,20 @@
 import "../css/PassRecovery.css"
+import { BsArrowLeftShort } from "react-icons/bs"
 
 function PassRecovery() {
     return (
-        <div>Hello World</div>
+        <div className='recovery-wrapper'>
+            <div className='recovery-card'>
+                <span><BsArrowLeftShort /></span>
+                <h1>Forget Password</h1>
+                <form action="">
+                    <div className='text-field'>
+                        <input type="email" name="email" placeholder='E-mail' required/>
+                    </div>
+                    <button type="submit">Continue</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
