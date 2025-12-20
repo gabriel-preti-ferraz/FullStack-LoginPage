45f5c030c52318cdc137fc4f3147fea803dc722c
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-19 21:45:43 -0300

    feat: PassRecovery css

--- frontend/src/pages/PassRecovery.jsx
+++ frontend/src/pages/PassRecovery.jsx
@@ -5,14 +5,16 @@ function PassRecovery() {
     return (
         <div className='recovery-wrapper'>
             <div className='recovery-card'>
-                <span><BsArrowLeftShort /></span>
                 <h1>Forget Password</h1>
                 <form action="">
                     <div className='text-field'>
+                        <label>Enter your e-mail address to continue</label>
                         <input type="email" name="email" placeholder='E-mail' required/>
                     </div>
                     <button type="submit">Continue</button>
                 </form>
+                <div className='divider'><span>or</span></div>
+                <p id="return">Return to the <a href=" /login">login page</a></p>
             </div>
         </div>
     )
