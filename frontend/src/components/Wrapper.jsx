814657b42e0d4f755d74109e1d4661029a7a2ce5
Gabriel Preti Ferraz <gferraz381@gmail.com>
2025-12-22 18:15:08 -0300

    feat: cardProps

--- frontend/src/components/Wrapper.jsx
+++ frontend/src/components/Wrapper.jsx
@@ -1,9 +1,9 @@
 import "../css/components/Wrapper.css"
 
-function Wrapper({ children, headerContent, footContent }) {
+function Wrapper({ children, headerContent, footContent, cardProps }) {
     return (
         <div className="wrapper">
-            <div className="card">
+            <div className="card" {...cardProps}>
                 {headerContent}
                 {children}
                 {footContent}
